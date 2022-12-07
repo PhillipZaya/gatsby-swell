36eccdf000734400c6ed0cf6997ab27a2212d230
Phillip Zaya <Phillip.zaya@outlook.com>
2022-12-07 14:29:09 +1100

    test

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import { Link } from 'gatsby'
+
+const Header = () => {
+    const [show,setShow] = useState(false)
+  return (
+    <nav className='navbar'>
+    <div className='nav-center'>
+      <div className='nav-header'>
+<Link to='/'>
+<h1>Barney</h1>
+</Link>
+<button className='nav-btn' onClick={()=> setShow(!show)}>
+</button>
+        </div>
+        <div className={show ? 'nav-links show-links' : 'nav-links'}>
+        <Link to='/' className='nav-link' activeClassName='active-link' onClick={()=> setShow(false)}>home</Link>
+        <Link to='/recipes' className='nav-link' activeClassName='active-link' onClick={()=> setShow(false)}>recipes</Link>
+        <Link to='/tags' className='nav-link' activeClassName='active-link' onClick={()=> setShow(false)}>tags</Link>
+        <Link to='/about' className='nav-link' activeClassName='active-link' onClick={()=> setShow(false)}>about</Link>
+        <div className='nav-link contact-link'>
+        <Link to='/contact' className='btn'>contact</Link>
+        </div>
+        </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
